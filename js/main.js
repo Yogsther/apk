0490b6f10a4e33a4a2050ff3d316638708ef102b
Olle Kaiser <olle.kaiser@learnet.se>
2020-01-28 10:52:58 +0100

    Alla kategori, Sökfunktion, Färgkoder

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@ var extraCategories = [];
 var pool = database["Öl"];
 var lastSearch = "";
 
+database["Alla"] = [];
+for (var key in database) {
+    if (key != "Alla") {
+        for (let item of database[key]) {
+            database["Alla"].push(item);
+        }
+    }
+}
+database["Alla"].sort((a, b) => {
+    return b.apk - a.apk;
+});
+
+var page = 0;
+var resultsPerPage = 10;
+
 var buttons = {
     Öl: "🍺",
     Cider: "🥂",
     "Vodka och Brännvin": "🥃",
     "Rött vin": "🍷",
     "Vitt vin": "🥂",
-    Likör: "🥃"
+    Likör: "🥃",
+    Alla: "🍻"
 };
 
 window.onload = () => {
@@ -22,11 +38,12 @@ for (var button in buttons) {
     }</button>`;
 }
 
-types.innerHTML += `<select onchange="changeType(this.value)" id="categories" class="type">
+/* types.innerHTML += `<select onchange="changeType(this.value)" id="categories" class="type">
 <option value="">Fler kategorier</option>
-</select>`;
+</select>`; */
 
 function changeType(type) {
+    page = 0;
     pool = database[type];
     if (buttons[type]) {
         document.getElementById("header").innerText = buttons[type];
@@ -46,27 +63,85 @@ for (var key in database) {
     if (Object.keys(buttons).indexOf(key) == -1) extraCategories.push(key);
 }
 
-for (var category of extraCategories) {
+/* for (var category of extraCategories) {
     document.getElementById(
         "categories"
     ).innerHTML += `<option value="${category}">${category}</option>`;
+} */
+
+function next() {
+    page++;
+    document.getElementById("back").disabled = false;
+
+    loadEntries();
 }
 
-function loadEntries(search = "", from = 0) {
+function back() {
+    page--;
+    if (page == 0) document.getElementById("back").disabled = true;
+    document.getElementById("next").disabled = false;
+    loadEntries();
+}
+
+var query = false;
+function search(q) {
+    query = q;
+    document.getElementById("navigation").style.display = query
+        ? "none"
+        : "block";
+
+    resultsPerPage = query ? 50 : 10;
+    loadEntries();
+}
+
+function getColor(apk) {
+    var score = apk * 110 - (database["Alla"][0].apk * 100 - 100);
+    if (score < 0) score = 0;
+
+    return `hsl(${score}, 91%, 62%)`;
+}
+
+function loadEntries() {
     var html = "";
-    for (var i = from; i < pool.length && i < from + 10; i++) {
+    var start = resultsPerPage * page;
+    var i = start;
+    var added = 0;
+
+    while (i < pool.length && added < resultsPerPage) {
         let item = pool[i];
-        html += `<div class="entry"> <span class="text">${item.name} </span>
-        <span class="description"><b>${item.apk} APK</b>
+        if (
+            !query ||
+            item.name.toLowerCase().indexOf(query.toLowerCase()) != -1
+        ) {
+            html += `<div class="entry"><div class="apk-label" style="background:${getColor(
+                item.apk
+            )};"></div> <span class="text">${item.name} </span>
+        <span class="description"><b >${item.apk} APK</b>
        ${item.price}kr / ${item.alc}% / ${item.volume}ml</span>
-        <a href="https://www.systembolaget.se/${item.nr}" target="_blank" class="sleve">
+        <a href="https://www.systembolaget.se/${
+            item.nr
+        }" target="_blank" class="sleve">
 
             <img src="img/systembolaget.png" class="store-link">
         </a>
-        </div>`;
+		</div>`;
+            added++;
+        }
+        i++;
     }
 
+    document.getElementById("next").disabled =
+        page * resultsPerPage > pool.length || added < resultsPerPage;
+
     document.getElementById("entries").innerHTML = html;
+    document.getElementById("page").innerText =
+        (page + 1).toString() +
+        " / " +
+        (added < resultsPerPage
+            ? page + 1
+            : query
+            ? "?"
+            : Math.ceil(pool.length / resultsPerPage));
 }
 
 loadEntries();
